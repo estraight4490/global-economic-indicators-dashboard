634ada3a1397e3c1e8fc59c5a79447c9cd755e17
Erik Straight <estraight1264@gmail.com>
2025-06-27 16:44:06 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+main.py
+
+Description:
+    A global economic indicators dashboard.
+
+Author:
+    Erik Straight <estraight1264@gmail.com>
+
+Created:
+    2025-06-27
+
+Python Version:
+    3.13.5
+
+Dependencies:
+    - pandas
+    - numpy
+    - matplotlib
+    (or just say: see requirements.txt)
+
+Usage:
+    python main.py
+
+Notes:
+    - Any setup instructions
+    - TODOs
+    - Known limitations
+"""
+
