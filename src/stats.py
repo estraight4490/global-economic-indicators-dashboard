b43cbb1e19c11f80101942f295e980e4daea6db0
Erik Straight <estraight1264@gmail.com>
2025-06-30 19:51:27 -0400

    Add correlation_analysis function

---
+++ src/stats.py
@@ -0,0 +1,10 @@
+import logging
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+def correlation_analysis(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.Series:
+    try:
+        return df1.T.corrwith(df2.T, method="pearson")
+    except Exception as e:
+        logger.error(f"correlation_analysis: Unexpected error: {e}")
